9e656f4b68f6f80a69af7f767da575807a97ab80
Yan Justino <yanlimaj@gmail.com>
2014-11-27 21:35:40 -0300

    Adicionando Application e DataModel

---
+++ Vacinas.Application/ServiceLocator.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Vacinas.DataAccess;
+using Vacinas.DataModel;
+using Vacinas.DataModel.Contracts;
+
+namespace Vacinas.Application
+{
+    public class ServiceLocator
+    {
+        public static ICRUD<doenca> CrudDoenca
+        {
+            get { return new CRUD<doenca>(); }
+        }
+    }
+}
---
+++ Vacinas.DataModel/Contracts/ICRUD.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vacinas.DataModel.Contracts
+{
+    public interface ICRUD<T> : IDisposable where T: class
+    {
+        List<T> Listar();
+        T Buscar(int id);
+        void Criar(T model);
+        void Excluir(int id);
+        void Alterar(T model);
+        void Salvar();
+    }
+}
